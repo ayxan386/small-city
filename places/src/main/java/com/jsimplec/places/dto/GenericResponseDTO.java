27aadf8bf64d07d469721976e792cdcd49399e04
ayxan386 <ayxanpro@gmail.com>
2021-02-17 20:00:05 +0400

    implemented get all places API

--- places/src/main/java/com/jsimplec/places/dto/GenericResponseDTO.java
+++ places/src/main/java/com/jsimplec/places/dto/GenericResponseDTO.java
@@ -1,2 +1,20 @@
-package com.jsimplec.places.dto;public class GenericResponseDTO<T> {
+package com.jsimplec.places.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenericResponseDTO<T> {
+  private static final String SUCCESS_MESSAGE = "success";
+  private T data;
+  private String message;
+
+  public static <T> GenericResponseDTO<T> success(T data) {
+    return GenericResponseDTO.<T>builder().data(data).message(SUCCESS_MESSAGE).build();
+  }
 }
