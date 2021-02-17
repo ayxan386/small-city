d2b49cff43d7f00ce9ec666f8bb7b30c8c871e1d
ayxan386 <ayxanpro@gmail.com>
2021-02-17 20:04:33 +0400

    implemented add place API

--- places/src/main/java/com/jsimplec/places/controller/PlaceController.java
+++ places/src/main/java/com/jsimplec/places/controller/PlaceController.java
@@ -1,13 +1,12 @@
 package com.jsimplec.places.controller;
 
 import com.jsimplec.places.dto.GenericResponseDTO;
+import com.jsimplec.places.dto.places.PlaceRequestDTO;
 import com.jsimplec.places.dto.places.PlaceResponseDTO;
 import com.jsimplec.places.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +23,13 @@ public class PlaceController {
     List<PlaceResponseDTO> allPlaces = placeService.getAll();
     log.info("Getting all places was successful");
     return GenericResponseDTO.success(allPlaces);
+  }
+
+  @PostMapping
+  public GenericResponseDTO<PlaceResponseDTO> addPlace(@RequestBody PlaceRequestDTO req) {
+    log.info("Adding new place {}", req.getName());
+    PlaceResponseDTO response = placeService.addPlace(req);
+    log.info("Place successfully added {}", response.getId());
+    return GenericResponseDTO.success(response);
   }
 }
