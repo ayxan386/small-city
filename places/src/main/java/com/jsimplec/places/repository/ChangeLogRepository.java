8e7b24af8fd0e351cec4cebb1e18ea6ee47879c2
ayxan386 <ayxanpro@gmail.com>
2021-02-21 19:59:36 +0400

    Added sorting to logs

--- places/src/main/java/com/jsimplec/places/repository/ChangeLogRepository.java
+++ places/src/main/java/com/jsimplec/places/repository/ChangeLogRepository.java
@@ -1,11 +1,12 @@
 package com.jsimplec.places.repository;
 
 import com.jsimplec.places.model.EntityChangeLogModel;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ChangeLogRepository extends CrudRepository<EntityChangeLogModel, UUID> {
-  List<EntityChangeLogModel> findAll();
+public interface ChangeLogRepository extends JpaRepository<EntityChangeLogModel, UUID> {
+  List<EntityChangeLogModel> findAll(Sort sort);
 }
