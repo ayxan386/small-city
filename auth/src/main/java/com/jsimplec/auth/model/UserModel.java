fdfe37cc00a275d538681120c4372a8971b28f5b
ayxan386 <ayxanpro@gmail.com>
2021-02-24 21:14:36 +0400

    Added basic UserModel

---
+++ auth/src/main/java/com/jsimplec/auth/model/UserModel.java
@@ -0,0 +1,36 @@
+package com.jsimplec.auth.model;
+
+import com.jsimplec.auth.constants.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "small_city_user")
+public class UserModel {
+  @Id
+  @GeneratedValue(strategy = AUTO)
+  private UUID id;
+  private String username;
+  private String email;
+  private String password;
+  @Enumerated(EnumType.STRING)
+  private UserStatus status;
+  @CreationTimestamp
+  private LocalDateTime createDate;
+  @UpdateTimestamp
+  private LocalDateTime updateDate;
+}
