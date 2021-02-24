028fbbd9cd3275fef3cc8286be93600ca8c7d653
ayxan386 <ayxanpro@gmail.com>
2021-02-24 21:21:43 +0400

    Added base DTOs for registration

---
+++ auth/src/main/java/com/jsimplec/auth/dto/register/RegisterResponseDTO.java
@@ -0,0 +1,14 @@
+package com.jsimplec.auth.dto.register;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponseDTO {
+  private String token;
+}
