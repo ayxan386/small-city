c45e7950a39e612637997dface3f1b1e8ee81b67
ayxan386 <ayxanpro@gmail.com>
2021-02-28 20:53:01 +0400

    Added verification model

---
+++ auth/src/main/java/com/jsimplec/auth/model/VerificationModel.java
@@ -0,0 +1,32 @@
+package com.jsimplec.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "small_city_verification")
+public class VerificationModel {
+  @Id
+  @GeneratedValue(strategy = AUTO)
+  private UUID id;
+  private UUID verificationId;
+  private UUID userId;
+  @CreationTimestamp
+  private LocalDateTime createdDate;
+}
---
+++ auth/src/main/java/com/jsimplec/auth/repository/VerificationRepository.java
@@ -0,0 +1,11 @@
+package com.jsimplec.auth.repository;
+
+import com.jsimplec.auth.model.VerificationModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VerificationRepository extends CrudRepository<VerificationModel, UUID> {
+}
