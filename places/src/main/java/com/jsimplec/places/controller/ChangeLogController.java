4cf95076c5d07631e79d06d03f33d158ffee9415
ayxan386 <ayxanpro@gmail.com>
2021-03-03 16:01:36 +0400

    Refactored Excel utils

--- places/src/main/java/com/jsimplec/places/controller/ChangeLogController.java
+++ places/src/main/java/com/jsimplec/places/controller/ChangeLogController.java
@@ -2,11 +2,16 @@ package com.jsimplec.places.controller;
 
 import com.jsimplec.places.dto.log.LogResponseDTO;
 import com.jsimplec.places.service.ChangeLogService;
+import com.jsimplec.places.service.PlaceChangeLogExporter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.util.List;
 
@@ -17,10 +22,20 @@ import java.util.List;
 public class ChangeLogController {
 
   private final ChangeLogService repository;
+  private final PlaceChangeLogExporter logExporter;
 
   @GetMapping
   public List<LogResponseDTO> getAll() {
     return repository.getAll();
+  }
+
+  @GetMapping("/excel")
+  public ResponseEntity<StreamingResponseBody> getAllExcel() {
+    return ResponseEntity
+        .ok()
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .header(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=\"myfilename.xlsx\"")
+        .body(s -> logExporter.getAllLogs().write(s));
   }
 
 }
--- places/src/main/java/com/jsimplec/places/util/ExcelUtils.java
+++ places/src/main/java/com/jsimplec/places/util/ExcelUtils.java
@@ -1,11 +1,10 @@
 package com.jsimplec.places.util;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
@@ -16,10 +15,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequiredArgsConstructor
 public class ExcelUtils {
 
+  private HSSFCellStyle commonStyle;
+  private HSSFCellStyle emptyCellStyle;
+
   public <T extends ExportableDto> Workbook exportToExcel(T data) {
     HSSFWorkbook workbook = new HSSFWorkbook();
+    initCellStyles(workbook);
     return addNewSheet(data, workbook);
   }
+
 
   public <T extends ExportableDto> Workbook addNewSheet(T data, Workbook workbook) {
     Sheet sheet = workbook.createSheet(data.getSheetName());
@@ -33,16 +37,18 @@ public class ExcelUtils {
 
   private <T extends ExportableDto> void addTableData(T data, Sheet sheet) {
     AtomicInteger rowNumber = new AtomicInteger(1);
-    Map<String, Integer> columnOrder = data.getColumnOrder();
     data
         .getData()
         .forEach(map -> {
           Row currentRow = sheet.createRow(rowNumber.getAndAdd(1));
-          map
-              .forEach((fieldName, value) -> {
-                Integer columnIndex = columnOrder.get(fieldName);
-                Cell cell = currentRow.createCell(columnIndex);
-                cell.setCellValue(ObjectUtils.nullSafeToString(value));
+          data
+              .getColumnOrder()
+              .forEach((fieldName, columnNumber) -> {
+                Object value = map.getOrDefault(fieldName, "----");
+                String stringValue = ObjectUtils.nullSafeToString(value);
+                Cell cell = currentRow.createCell(columnNumber);
+                cell.setCellStyle(stringValue.equals("----") ? emptyCellStyle : commonStyle);
+                cell.setCellValue(stringValue);
               });
         });
   }
@@ -50,6 +56,13 @@ public class ExcelUtils {
   private void addHeader(Sheet sheet, Map<String, Integer> columnOrder) {
     Row headers = sheet.createRow(0);
     columnOrder.forEach((fieldName, index) -> headers.createCell(index).setCellValue(fieldName));
+  }
+
+  private void initCellStyles(HSSFWorkbook workbook) {
+    commonStyle = workbook.createCellStyle();
+    commonStyle.setAlignment(HorizontalAlignment.RIGHT);
+    emptyCellStyle = workbook.createCellStyle();
+    emptyCellStyle.setFillBackgroundColor(HSSFColor.HSSFColorPredefined.RED.getIndex());
   }
 
 }
