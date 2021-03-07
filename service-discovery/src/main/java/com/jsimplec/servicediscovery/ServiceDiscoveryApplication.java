60e308a343445295d2fc161f5a772680fb992d49
ayxan386 <ayxanpro@gmail.com>
2021-03-07 22:03:41 +0400

    Added configuration for Eureka server

--- service-discovery/src/main/java/com/jsimplec/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/jsimplec/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.jsimplec.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceDiscoveryApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceDiscoveryApplication.class, args);
+  }
 
 }
