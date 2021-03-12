c4d7c29f9ef1f2cb1f2b8ca724b7c0dd6fe0f6e5
ayxan386 <ayxanpro@gmail.com>
2021-03-12 22:03:40 +0400

    Added caching to get-all pr-plans service

---
+++ pr-ms/src/main/java/com/jsimplec/prms/config/RedisConfiguration.java
@@ -0,0 +1,11 @@
+package com.jsimplec.prms.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class RedisConfiguration {
+
+
+}
