ec97ae2e3a6b7bb52c769739e331e7ccdced1969
ayxan386 <ayxanpro@gmail.com>
2021-03-15 21:30:41 +0400

    Implemented auth filter with proper errors

--- pr-ms/src/main/java/com/jsimplec/prms/errors/ErrorDefinition.java
+++ pr-ms/src/main/java/com/jsimplec/prms/errors/ErrorDefinition.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorDefinition {
   NOT_IMPLEMENTED("Called method is not implemented", 502, "not_implemented_method"),
   PR_PLAN_ALREADY_EXISTS("PR plan with name %s already exists", 400, "pr_plan_exists"),
-  INVALID_METHOD_ARGUMENT("Invalid method argument: %s", 400, "invalid_method_argument");
+  INVALID_METHOD_ARGUMENT("Invalid method argument: %s", 400, "invalid_method_argument"),
+  TOKEN_EXPIRED("Token expired", 401, "token_expired"),
+  WRONG_HEADER_STRUCTURE("Invalid header structure", 400, "wrong_header"),
+  AUTH_HEADER_NOT_PRESENT("Authorization header is not present", 401, "auth_header_missing");
   private final String message;
   private final int status;
   private final String localKey;
