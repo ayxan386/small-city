5986977014c03f28fadd03b081e112906784b378
ayxan386 <ayxanpro@gmail.com>
2021-03-20 20:30:44 +0400

    Updated redis configuration

--- pr-ms/src/main/java/com/jsimplec/prms/config/RedisConfiguration.java
+++ pr-ms/src/main/java/com/jsimplec/prms/config/RedisConfiguration.java
@@ -1,11 +1,24 @@
 package com.jsimplec.prms.config;
 
+import com.jsimplec.prms.model.PaymentRedisModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @EnableCaching
 @Configuration
 public class RedisConfiguration {
 
+  @Value("${spring.cache.redis.hostname}")
+  private String hostname;
 
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(new RedisStandaloneConfiguration(hostname));
+  }
 }
